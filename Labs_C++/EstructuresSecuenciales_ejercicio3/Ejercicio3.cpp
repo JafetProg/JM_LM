94abc4b66fa5a42bbf52d8c308573a570902bef0
Jafet Melendez <jafetmelendez07@gmail.com>
2024-03-20 22:10:08 -0400

    "Estructuras Secuenciales"

---
+++ Labs_C++/EstructuresSecuenciales_ejercicio3/Ejercicio3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+int currentYear, BirthYear;
+cout << "Input the current year: ";
+cin >> currentYear;
+cout << "Input the year of your birth: ";
+cin >> BirthYear;
+int Age = currentYear - BirthYear;
+cout << "Your age is: " << Age;
+}
